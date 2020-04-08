5cae107645a57f3cd5bfe4e159750dde5f37f3cc
Sanna Aawa <sanna@aawacreative.com>
2020-04-08 11:08:15 +0300

    visualizing data

--- myChart.js
+++ myChart.js
@@ -28,5 +28,43 @@ async function fetchData() {
     if (response.status == 200) {
         var fetchedData = await response.json();
         console.log(fetchedData);
+        
+        var data = getValues(fetchedData);
+        var labels = getLabels(fetchedData);
+        var countryName = getCountryName(fetchedData);
+        renderChart(data, labels, countryName);
     }
+}
+
+function getValues(data) {
+    var vals = data[1].sort((a, b) => a.date - b.date).map(item => item.value);
+    return vals;
+}
+
+function getLabels(data) {
+    var labels = data[1].sort((a, b) => a.date - b.date).map(item => item.date);
+    return labels;
+}
+
+function getCountryName(data) {
+    var countryName = data[1][0].country.value;
+    return countryName;
+}
+
+function renderChart(data, labels, countryName) {
+    var ctx = document.getElementById('myChart').getContext('2d');
+    
+    // Draw new chart
+    new Chart(ctx, {
+        type: 'line',
+        data: {
+            labels: labels,
+            datasets: [{
+                label: 'Population, ' + countryName,
+                data: data,
+                borderColor: 'rgba(75, 192, 192, 1)',
+                backgroundColor: 'rgba(75, 192, 192, 0.2)',
+            }]
+        }
+    });
 }
